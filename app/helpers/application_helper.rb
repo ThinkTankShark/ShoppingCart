2ebe4c7329b97ce8194e64b7cef1fc0400a0cf73
Ronu Ghoshal <email": null>
2016-04-12 20:13:34 -0700

    Add some testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
 end
+
+module AuthHelper
+  def http_login
+    user = 'admin'
+    pw = 'secret'
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
+  end
+end
+
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
 
-    before do
-      @all_products = 3.times.map do
-        Product.create!(title: "Title", description: "Desc", price: rand(1..100))
-      end
-    end
+
+
+
+  before do
+      http_login
+  end
 
 
   describe "#index" do
 
     it "should load all products" do
+      @all_products = 3.times.map do
+        Product.create!(title: "Title", description: "Desc", price: rand(1..100))
+      end
       get :index
       expect(assigns[:products]).to eq @all_products
       expect(response.status).to eq 200
@@ -20,12 +24,15 @@ RSpec.describe ProductsController, type: :controller do
   end
 
   describe "#create" do
+
     context "when given valid params" do
       let (:params) {{product: {title: "My title", description: "A valid title", price: 5}}}
       it "should increment products in the database by 1" do
+
         expect{post :create, params}.to change{Product.count}.by(1)
       end
       it "responds with a status of 302" do
+
         post :create, params
         expect(response.status).to eq 302
       end
@@ -43,15 +50,14 @@ RSpec.describe ProductsController, type: :controller do
   end
 
   describe "#update" do
-    pending
-    # let!(:product) { Product.create(title: "My title", description: "A valid title", price: 5) }
-    # context 'on valid params' do
-    #   let(:params) {{product: {title: product.title, description: "new description", id: 1}}}
-    #   it 'responds with a status of 302' do
-    #     patch :update, params
-    #     expect(response.status).to eq(302)
-    #   end
-    # end
+    let!(:old_product) { FactoryGirl.create(:product) }
+    context 'on valid params' do
+      let(:params) {{product: {title: old_product.title, description: "new description", price: old_product.price, id: old_product.id}}}
+      it 'responds with a status of 302' do
+        patch :update, params
+      expect(response.status).to eq(302)
+      end
+    end
   end
 
   describe "#destroy" do
