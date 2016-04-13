5c4732a9e2bc9830936e8ba1861a837c3efc03fb
Ronu Ghoshal & Sharon <pair+RonuGhoshal.sharonjean@devbootcamp.com>
2016-04-12 17:17:31 -0700

    Create Admin inventory management dashboard view.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
   http_basic_authenticate_with name: "admin", password: "secret"
 
   def dashboard
-    @foo = "bar"
+    @products = Product.all
   end
 end
