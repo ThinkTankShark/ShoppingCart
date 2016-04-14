6f86aaf1f712c438d929f58ae9fb8a431f9d2340
Ronu Ghoshal <email": null>
2016-04-14 08:50:26 -0700

    Update categories spec

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -9,22 +9,14 @@ RSpec.feature "Products", js: true do
 
 
   describe 'filling out a form' do
-    it 'can create a new product and see it rendered on the page' do
+    it '' do
 
     end
   end
 
   describe 'editing an existing product' do
-    it 'can fill out the form of the product page and see updated result' do
-      visit edit_product_path(new_product.id)
-      title = "edit title"
-      fill_in 'Title', with: title
-      price = 36.00
-      fill_in 'Price', with: price
-      body = "Here is edit text"
-      fill_in 'Description', with: body
-      click_button 'Update Product'
-      expect(page).to have_content(title)
+    it '' do
+
     end
   end
 end
