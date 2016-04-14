412d80007967f95e75f64ef2a08c3bd79bf5e31e
Ronu Ghoshal <email": null>
2016-04-14 07:30:07 -0700

    Add application file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,5 @@
 Rails.application.configure do
 
-  config.action_mailer_delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -88,4 +78,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer_delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+    }
+
 end
