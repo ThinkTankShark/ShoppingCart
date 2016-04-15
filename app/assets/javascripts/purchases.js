0a0fd314247710b4f4ad5a5f06713af224f2a65b
Cyrus & Sharon <pair+cyvattes.sharonjean@devbootcamp.com>
2016-04-15 15:04:33 -0700

    Complete Shopping cart functionality.

--- app/assets/javascripts/purchases.js
+++ app/assets/javascripts/purchases.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $('#add-to-cart-form').on('ajax:success',function(event, response) {
+      $("#cart-num").text(response);
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  resources :purchases
   root :to => 'products#index'
 
   get 'admin' => 'admin#dashboard'
-
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  get 'cart' => 'purchases#cart'
+  post 'checkout' => 'purchases#create'
 
   resources :products
   resources :users
   resources :categories
-
+  resources :purchases
 end
