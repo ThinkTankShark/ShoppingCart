280234a3c07a0bcd4aac9fabdb81a4e5a839dfb1
Ronu Ghoshal & Sam Assadi <pair+RonuGhoshal.ThinkTankShark@devbootcamp.com>
2016-04-14 18:09:33 -0700

    Create basic ajax req/resp for Add To Cart

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,18 +19,27 @@ class SessionsController < ApplicationController
 
   def shop
     @product = Product.find(params[:product_id])
-    p @product.title
     if !session[:cart]
       session[:cart] = {}
     end
     if session[:cart][@product.id.to_s]
-        session[:cart][@product.id.to_s] += 1
-        p session[:cart]
+      session[:cart][@product.id.to_s] += 1
     else
-        session[:cart][@product.id.to_s] = 1
-        p session[:cart]
+      session[:cart][@product.id.to_s] = 1
     end
-    redirect_to '/'
+    @cart = session[:cart]
+    @subtotal = 0
+    @num_in_cart = 0
+    @cart.each do |id, qty|
+      @subtotal += (Product.find(id).price * qty)
+      @num_in_cart += qty
+    end
+
+    if request.xhr?
+      render '_subtotal', layout: false
+    else
+      render '_cart'
+    end
   end
 
 end
