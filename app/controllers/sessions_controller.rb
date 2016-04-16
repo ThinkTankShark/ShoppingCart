a9450683df9301a01cd6ef74ef01a55de810a9e8
Ronu Ghoshal <email": null>
2016-04-16 13:38:37 -0700

    Add order history

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:id] = nil
+    session.clear
     redirect_to products_path
   end
 
@@ -55,52 +55,25 @@ class SessionsController < ApplicationController
     end
   end
 
-    # if !session[:stock][product_id]
-    #   session[:stock] = {product_id => @product.quantity-1}
-    # end
-    # if session[:stock][product_id] > 0
-    #   if session[:cart][product_id]
-    #     session[:cart][product_id] += 1
-    #   else
-    #     session[:cart][product_id] = 1
-    #   end
-    #   session[:stock][product_id] -= 1
-    #   @cart = session[:cart]
-    #   @subtotal = 0
-    #   @num_in_cart = 0
-    #   @cart.each do |id, qty|
-    #     @subtotal += (Product.find(id).price * qty)
-    #     @num_in_cart += qty
-    #   end
-    #   p session[:stock]
-    #   render '_subtotal', layout: false
-    # else
-    #   flash[:danger] = "This item is currently out of stock."
-    #   render 'products/index'
-    # end
-
-
-
-    def remove_item
-      id = params[:id]
-      session[:cart][id] -= 1
-      price = Product.find(id).price
-      if request.xhr?
-        data = {id: id, price: price, qty: session[:cart][id]}
-        render :json => data, :status => :ok
-      else
-        render '_cart'
-      end
+  def remove_item
+    id = params[:id]
+    session[:cart][id] -= 1
+    price = Product.find(id).price
+    if request.xhr?
+      data = {id: id, price: price, qty: session[:cart][id]}
+      render :json => data, :status => :ok
+    else
+      render '_cart'
     end
+  end
 
-    def view_cart
-      @cart = session[:cart]
-      if request.xhr?
-        render '_subtotal', layout: false
-      else
-        render '_cart'
-      end
+  def view_cart
+    @cart = session[:cart]
+    if request.xhr?
+      render '_subtotal', layout: false
+    else
+      render '_cart'
     end
-
+  end
 
 end
