600bc3d6c5ea586789dcbc48838dac4aa2b2c91f
Ronu Ghoshal <email": null>
2016-04-16 12:38:36 -0700

    Update cart and stock count - almost fully functional

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,22 +18,27 @@ class SessionsController < ApplicationController
   end
 
   def shop
+    @products = Product.all
     @product = Product.find(params[:product_id])
+    product_id = @product.id.to_s
     if !session[:cart]
       session[:cart] = {}
     end
     if !session[:stock]
-      session[:stock] = @product.quantity - 1
+      session[:stock] = {}
     end
-    if session[:stock] > 0
-      if session[:cart][@product.id.to_s]
-        session[:cart][@product.id.to_s] += 1
-        session[:stock] -= 1
-        p @product.id
-        p session[:stock]
+    if !session[:stock][product_id]
+      session[:stock][product_id] = @product.quantity
+    end
+    if session[:stock][product_id] > 0
+      if !session[:cart][product_id]
+        session[:cart][product_id] = 1
       else
-        session[:cart][@product.id.to_s] = 1
+        session[:cart][product_id] += 1
       end
+      session[:stock][product_id] -= 1
+      p session[:cart][product_id]
+      p session[:stock][product_id]
       @cart = session[:cart]
       @subtotal = 0
       @num_in_cart = 0
@@ -44,9 +49,35 @@ class SessionsController < ApplicationController
       render '_subtotal', layout: false
     else
       flash[:danger] = "This item is currently out of stock."
-      redirect_to '/'
+      render 'products/index'
     end
   end
+
+    # if !session[:stock][product_id]
+    #   session[:stock] = {product_id => @product.quantity-1}
+    # end
+    # if session[:stock][product_id] > 0
+    #   if session[:cart][product_id]
+    #     session[:cart][product_id] += 1
+    #   else
+    #     session[:cart][product_id] = 1
+    #   end
+    #   session[:stock][product_id] -= 1
+    #   @cart = session[:cart]
+    #   @subtotal = 0
+    #   @num_in_cart = 0
+    #   @cart.each do |id, qty|
+    #     @subtotal += (Product.find(id).price * qty)
+    #     @num_in_cart += qty
+    #   end
+    #   p session[:stock]
+    #   render '_subtotal', layout: false
+    # else
+    #   flash[:danger] = "This item is currently out of stock."
+    #   render 'products/index'
+    # end
+
+
 
     def remove_item
       id = params[:id]
